14824b42f285759ad751e54d3f5ef1cd3305a214
Rachel <rdalby@hsph.harvard.edu>
2021-11-05 12:20:13 -0400

    Initial commit

---
+++ Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemSpawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start () {
+
+		// infinite gem spawning function, asynchronous
+		StartCoroutine(SpawnGems());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator SpawnGems() {
+		while (true) {
+
+			// only spawn 1 gem in a column makes it more rare
+			// same instantiate as the coins except won't all be in the same row
+			Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+
+			// pause 3-20 seconds until the next gem spawns
+			yield return new WaitForSeconds(Random.Range(3, 20));
+		}
+	}
+}
